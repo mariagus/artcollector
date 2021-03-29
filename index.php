994682a2fab2c56c81dcf5167adbe9801d239e24
Maria Gusova <masha.gu@gmail.com>
2021-03-29 15:28:32 +0100

    Change foreach display and add title

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<h1>Art Collector App</h1>
+
 <?php
 
 require "getArtworks.php";
@@ -6,12 +8,14 @@ function displayArtworks()
 {
     $artworks = getArtworks();
     '<div class="artworks">';
+    $return = '';
     foreach ($artworks as $artwork){
-        echo '<h1>' . $artwork['title'] . '</h1>';
-        echo '<h2>' . $artwork['artist'] . '</h2>';
-        echo '<h3>' . $artwork['movement']. '</h3>';
-        echo '<h3>' . $artwork['year']. '</h3>';
+        $return .= '<h2>Title: ' . $artwork['title'] . '</h2>';
+        $return .= '<h3>Artist: ' . $artwork['artist'] . '</h3>';
+        $return .= '<h3>Movement: ' . $artwork['movement']. '</h3>';
+        $return .= '<h3>Year: ' . $artwork['year']. '</h3>';
     }
+    return $return;
     '</div>';
 
 }
