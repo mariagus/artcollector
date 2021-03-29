3d5b3c745beb4a1ca5cb975685847bdb971d0b55
Maria Gusova <masha.gu@gmail.com>
2021-03-29 13:33:57 +0100

    connect to DB and get all

---
+++ getArtworks.php
@@ -0,0 +1,17 @@
+<?php
+require "connectToDb.php";
+
+function getArtworks()
+{
+    $db = getDB();
+    $query = $db->prepare('SELECT * FROM `artworks`;');
+    $query->execute();
+    $artworks = $query->fetchAll();
+    return $artworks;
+}
+
+$artworks = getArtworks();
+
+echo '<pre>';
+var_dump($artworks);
+echo '</pre>';
