e8ace1e87f43d3655ef09e77c140e3692d7753c3
Maria Gusova <masha.gu@gmail.com>
2021-03-29 15:04:07 +0100

    Add displayArtworks function

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 require "getArtworks.php";
 
-$artworks = getArtworks();
+function displayArtworks()
+{
+    $artworks = getArtworks();
+    '<div class="artworks">';
+    foreach ($artworks as $artwork){
+        echo '<h1>' . $artwork['title'] . '</h1>';
+        echo '<h2>' . $artwork['artist'] . '</h2>';
+        echo '<h3>' . $artwork['movement']. '</h3>';
+        echo '<h3>' . $artwork['year']. '</h3>';
+    }
+    '</div>';
 
-echo '<pre>';
-var_dump($artworks);
-echo '</pre>';
+}
+
+echo displayArtworks();
