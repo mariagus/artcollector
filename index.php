054360d3c82ae8fea753553cd2d4a15cfbc775c5
Maria Gusova <masha.gu@gmail.com>
2021-03-30 16:33:43 +0100

    add labels

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 </head>
 <body>
 <h1>Art Collector App</h1>
-<form action="form.php" method="post">
-    Title: <input type="text" name="title" />
-    Artist: <input type="text" name="artist" />
-    Movement: <input type="text" name="movement" />
-    Year: <input type="number" name="year" />
-    Image link: <input type="url" name="image" />
-    <input type="submit" />
-</form>
-
+<div>
+<h3>Add Artwork: </h3>
+    <form action="form.php" method="post">
+        <label>Title: <input type="text" name="title" /></label>
+        <label>Artist: <input type="text" name="artist" /></label>
+        <label>Movement: <input type="text" name="movement" /></label>>
+        <label>Year: <input type="number" name="year" /></label>
+        <label>Image link: <input type="url" name="image" /></label>
+        <input type="submit" />
+    </form>
 <?php
 if (isset($_GET['error'])) {
     echo '<p>Please fill in all fields!</p>';
 }
 ?>
-
+</div>
 <section class="container">
-
 <?php
 require "functions.php";
 require "connectToDb.php";
@@ -29,8 +29,8 @@ $db = getDB();
 $artworks = getArtworks($db);
 echo displayArtworks($artworks);
 ?>
-
 </section>
+
 </body>
 </html>
 
