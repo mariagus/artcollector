0100f9d35f5bccdca809ce9e2d0890b9d4a8049f
Maria Gusova <masha.gu@gmail.com>
2021-03-30 22:58:29 +0100

    style form

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <html>
 <head>
+    <title>Art Collector App</title>
     <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
 <h1>Art Collector App</h1>
-<div>
+<div class="form">
 <h3>Add Artwork: </h3>
-    <form action="form.php" method="post">
+    <form action="form.php" method="post" >
         <label>Title: <input type="text" name="title" /></label>
         <label>Artist: <input type="text" name="artist" /></label>
-        <label>Movement: <input type="text" name="movement" /></label>>
+        <label>Movement: <input type="text" name="movement" /></label>
         <label>Year: <input type="number" name="year" /></label>
-        <label>Image link: <input type="url" name="image" /></label>
-        <input type="submit" />
+        <label>Image url: <input type="url" name="image" /></label>
+        <input type="submit" class="submit"/>
     </form>
 <?php
-
 if (isset($_GET['error'])) {
-    echo '<p>Please fill in all fields!</p>';
+    echo '<p class="warning">Please fill in all fields!</p>';
 }
 ?>
 </div>
