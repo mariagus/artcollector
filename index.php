bf2deb042b34832c0a9ceeb94f13d288ec4fb188
Maria Gusova <masha.gu@gmail.com>
2021-03-30 09:41:10 +0100

    create basic form

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
 </head>
 <h1>Art Collector App</h1>
 <body>
+<form action="form.php" method="post">
+    <label>Title: </label>
+    <input type="text" name="title" />
+    <label>Artist: </label>
+    <input type="text" name="artist" />
+    <label>Movement: </label>
+    <input type="text" name="movement" />
+    <label>Year: </label>
+    <input type="number" name="year" />
+    <label>Image url: </label>
+    <input type="url" name="image" />
+    <input type="submit" />
+</form>
 <section class="container">
 
 <?php
