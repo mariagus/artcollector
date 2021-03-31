7bbff68383cc0025ea6afe9f64c559b79b1716d6
Maria Gusova <masha.gu@gmail.com>
2021-03-31 14:19:52 +0100

    add html

--- form.php
+++ form.php
@@ -1,3 +1,12 @@
+<html>
+<head>
+    <title>Art Collector App</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+<body>
+</body>
+</html>
 <?php
 
 require_once 'connectToDb.php';
--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function displayArtworks(array $data) : string
             $return .= '<img src="' . $artwork['image'] . '" height="250" />';
             $return .= '<h2>Title: ' . $artwork['title'] . '</h2>';
             $return .= '<h3>Artist: ' . $artwork['artist'] . '</h3>';
-            $return .= '<h4>Movement: ' . $artwork['movement'] . '</h4>';
+            $return .= '<h3>Movement: ' . $artwork['movement'] . '</h3>';
             $return .= '<h4>Year: ' . $artwork['year'] . '</h4>';
             $return .= '<a href="delete.php?id=' . $artwork['id'] . '">delete</a>';
             $return .= '</div>';
